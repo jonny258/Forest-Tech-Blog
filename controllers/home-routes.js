f305d9e728b9d4f54cc8b27fd3a61325a1b39373
jonny desktop <jon@uslms.com>
2023-05-27 14:10:39 -0600

    made the handlebar templates work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,68 @@
 const router = require('express').Router();
 
+//test database
+
+const blogData = [
+    {
+      author: 'John Doe',
+      title: 'Introduction to JavaScript',
+      body: 'JavaScript is a powerful programming language used for web development.',
+    },
+    {
+      author: 'Jane Smith',
+      title: 'Mastering CSS',
+      body: 'CSS allows you to style and layout web pages with ease.',
+    },
+    {
+      author: 'Alex Johnson',
+      title: 'The Art of HTML',
+      body: 'HTML is the foundation of every webpage on the internet.',
+    },
+    {
+      author: 'Sarah Williams',
+      title: 'Deep Dive into React',
+      body: 'React is a popular JavaScript library for building user interfaces.',
+    },
+    {
+      author: 'Michael Brown',
+      title: 'Python for Data Science',
+      body: 'Python is widely used in the field of data science and analysis.',
+    },
+    {
+      author: 'Emily Davis',
+      title: 'Exploring UX Design',
+      body: 'UX design focuses on enhancing user satisfaction with a product.',
+    },
+    {
+      author: 'David Johnson',
+      title: 'Getting Started with Node.js',
+      body: 'Node.js allows for server-side JavaScript development.',
+    },
+    {
+      author: 'Sophia Martinez',
+      title: 'Mastering Responsive Web Design',
+      body: 'Responsive web design ensures websites adapt to different devices.',
+    },
+    {
+      author: 'Robert Wilson',
+      title: 'The Art of Algorithms',
+      body: 'Understanding algorithms is crucial for efficient problem-solving.',
+    },
+    {
+      author: 'Olivia Anderson',
+      title: 'Exploring Data Visualization',
+      body: 'Data visualization helps communicate insights through visuals.',
+    }
+  ];
+  
+
+//end of test database
+  
+
 router.get('/', (req, res) => {
-    res.json('testing')
+    res.render('homepage', {
+        blogData,
+    });
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express')
-const app = express()
+const path = require('path')
+
+const exphbs = require('express-handlebars')
 
 const routes = require('./controllers')
 const sequelize = require('./config/connection')
 
+const app = express()
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create()
+
+//comment on what these do
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+//comment on what all these do
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes)
 
 
 sequelize.sync({ force: false}).then(() => {
-    app.listen(PORT, () => console.log('site lvie at http://localhost:3001/'))
+    app.listen(PORT, () => console.log('site live at http://localhost:3001/'))
 })
