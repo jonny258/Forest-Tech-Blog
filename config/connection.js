b144c05ab56ad3ff79cb86fa0e78dfff61c67c9c
jonny desktop <jon@uslms.com>
2023-06-11 12:37:57 -0600

    Remove nested git repository

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize'); //gets the sqeualize npm package
 require('dotenv').config(); //this is what you need to get the .env file to work
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME, //these are the variables that are saved in the .env files
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+
+let sequelize;
+if(process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+}else{
+  sequelize = new Sequelize(
+    process.env.DB_NAME, //these are the variables that are saved in the .env files
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
 
 module.exports = sequelize;
